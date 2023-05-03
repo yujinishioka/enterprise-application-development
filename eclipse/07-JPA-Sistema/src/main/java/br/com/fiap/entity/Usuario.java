7785a56692c08192ae3e3a3a28229602dc930aac
Vinicius Yuji Nishioka <yuu.nishioka@outlook.com>
2023-05-03 19:28:30 -0300

    Feat: Entitades de projeto 07-JPA

--- eclipse/07-JPA-Sistema/src/main/java/br/com/fiap/entity/Usuario.java
+++ eclipse/07-JPA-Sistema/src/main/java/br/com/fiap/entity/Usuario.java
@@ -1,9 +1,53 @@
 package br.com.fiap.entity;
 
-public class Usuario {
-	
-	private Integer id;
-	
-	private String nome;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TAB_USUARIO")
+@SequenceGenerator(name="usuario", sequenceName = "SQ_TAB_USUARIO", allocationSize = 1)
+public class Usuario {
+
+	@Id
+	@Column(name="cod_usuario")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "usuario")
+	private Integer codigo;
+	
+	@Column(name="nom_usuario")
+	private String nome;
+	
+	@ManyToMany(mappedBy = "usuarios")
+	private List<ItemTeste> itensTeste;
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<ItemTeste> getItensTeste() {
+		return itensTeste;
+	}
+
+	public void setItensTeste(List<ItemTeste> itensTeste) {
+		this.itensTeste = itensTeste;
+	}
 }
