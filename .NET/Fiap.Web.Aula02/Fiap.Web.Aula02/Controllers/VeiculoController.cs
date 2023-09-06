214ef2e87b48ccba404bf1521ef096ea1c1eabd1
Vinicius Yuji Nishioka <yuu.nishioka@outlook.com>
2023-09-06 20:47:36 -0300

    feat: projeto de locadora de carros

---
+++ .NET/Fiap.Web.Aula02/Fiap.Web.Aula02/Controllers/VeiculoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Fiap.Web.Aula02.Models;
+
+namespace Fiap.Web.Aula02.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Veiculo veiculo)
+        {
+            return View();
+        }
+    }
+}
---
+++ .NET/Fiap.Web.Aula02/Fiap.Web.Aula02/Models/Veiculo.cs
@@ -0,0 +1,13 @@
+﻿namespace Fiap.Web.Aula02.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public int Ano { get; set; }
+        public long Km { get; set; }
+        public string? Placa { get; set; }
+        public string? Modelo { get; set; }
+        public string? Marca { get; set; }
+        public bool Disponivel { get; set; }
+    }
+}
